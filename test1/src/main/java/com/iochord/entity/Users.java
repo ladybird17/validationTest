d076604d9f39dfe2ceee39b592cdac84a6b2bda4
ladybird17 <ladybird1762@gmail.com>
2021-06-16 14:55:42 +0900

    db연결 없애고 entity 완전 수정

---
+++ test1/src/main/java/com/iochord/entity/Users.java
@@ -0,0 +1,31 @@
+package com.iochord.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+public class Users {
+
+    @NotNull
+    @Size(min=2, max=6, message = "Size Error of userName 성 포함 최소 2자 최대 6자여야합니다")
+    String userName;
+
+    @NotNull
+    @NotBlank(message = "Blank Error of userEmail 이메일을 입력하세요")
+    @Size(max=30, message = "Size Error of userEmail 이메일 도메인 포함 최대 30자 이하여야합니다")
+    @Email
+    String userEmail;
+
+    @NotNull
+    @NotBlank(message = " Blank Error of userPhone 전화번호를 입력하세요")
+    @Pattern(regexp = "^01(?:[0-9])(\\d{3}|\\d{4})(\\d{4})$", message = "Pattern error of userPhone 전화번호는 숫자로만 입력하고, 10~11자여야 합니다.")
+    String userPhone;
+
+    @NotNull
+    @Min(value=0, message="Minimum error of userAge 나이는 최소 0살 이상이어야 합니다")
+    @Max(value=120, message="Maximum error of userAge 나이는 최대 120살 이하여야 합니다")
+    int userAge;
+}
--- test1/src/main/java/com/iochord/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iochord.repository;
-
-import com.iochord.entity.UsersEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRepository extends CrudRepository<UsersEntity, Integer>{
-
-
-}
